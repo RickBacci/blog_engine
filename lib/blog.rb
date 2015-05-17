649e28453a93d66065134b9cbcb1d04d2a3544b8
Rick Bacci <rickbacci@gmail.com>
2015-05-17 15:25:33 -0600

    initial commit

---
+++ lib/blog.rb
@@ -0,0 +1,47 @@
+require 'sinatra/base'
+require 'ostruct'
+require 'time'
+
+class Blog < Sinatra::Base
+  # File.expand_path generates an absolute path.
+  # It also takes a path as a second argument.
+  # The generated path is treated as being relative
+  # to that path.
+  set :root, File.expand_path('../../' __FILE__)
+
+  # loop through all the article files
+  Dir.glob "#{root}/articles/*.md" do |file|
+    # parse metadata and content from file
+    meta, content = File.read(file).split("\n\n", 2)
+
+    # generate a metadata object
+    article = OpenStruct.new YAML.load(meta)
+
+    # convert the date to a time object
+    article.date = Time.parse article.date.to_s
+
+    # add the content
+    article.content = content
+
+    # generate a slug for the url
+    article.slug = File.basename(file, '.md')
+
+    # set up the route
+
+    get "/#{article.slug}" do
+      erb :post, :locals => { :article => article }
+    end
+
+    # add article to the list of articles
+    articles << article
+  end
+
+  # sort articles by date, display new articles first
+  articles.sort_by! { |article| article.date }
+
+  get '/' do
+    erb :index
+  end
+end
+
+
